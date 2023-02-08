a355fba05a33b58b4cfe93e07d5bb6576766894b
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-08 09:45:58 -0300

    fix(react): removing strict mode, as it was breaking chart.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './app-routing';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <>
     <ToastContainer
       hideProgressBar
       toastClassName="notification__container"
@@ -13,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       closeButton={false}
     />
     <AppRouter />
-  </React.StrictMode>,
+  </>,
 );
