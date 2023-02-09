058ae48972b9fc73bfa6f99de1a654e45aeb09c4
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2023-02-09 14:18:35 -0300

    style: making sidepanel sticky

--- src/root/sidepanel.tsx
+++ src/root/sidepanel.tsx
@@ -16,41 +16,43 @@ function SidePanel() {
 
   return (
     <div className={`${styles.sidepanel} muralis-primary`}>
-      <section className={`${styles.sidepanel__profile} text-white`}>
-        <img src={accountFallback} alt="" className={`${styles.profile__picture} mb-3`} />
-        <p className="h3 text-center mb-0">
-          Guilherme L. Mouraes
-        </p>
-        <p className={styles.profile__email}>
-          guilherme.lmoraes.devel@gmail.com
-        </p>
-      </section>
-      <nav>
-        <ul className={styles['sidepanel__link-list']}>
-          <li>
-            <NavLink
-              to="/"
-              className={navLinkClasses}
-            >
-              <i className="bi bi-house-fill me-3" />
-              <span>
-                Home
-              </span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/ingressantes"
-              className={navLinkClasses}
-            >
-              <i className="bi bi-person-fill-add me-3" />
-              <span>
-                Ingressantes
-              </span>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.sidepanel__body}>
+        <section className={`${styles.sidepanel__profile} text-white`}>
+          <img src={accountFallback} alt="" className={`${styles.profile__picture} mb-3`} />
+          <p className="h3 text-center mb-0">
+            Guilherme L. Mouraes
+          </p>
+          <p className={styles.profile__email}>
+            guilherme.lmoraes.devel@gmail.com
+          </p>
+        </section>
+        <nav>
+          <ul className={styles['sidepanel__link-list']}>
+            <li>
+              <NavLink
+                to="/"
+                className={navLinkClasses}
+              >
+                <i className="bi bi-house-fill me-3" />
+                <span>
+                  Home
+                </span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/ingressantes"
+                className={navLinkClasses}
+              >
+                <i className="bi bi-person-fill-add me-3" />
+                <span>
+                  Ingressantes
+                </span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
